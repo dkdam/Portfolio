2ddf66851e497d2af911431f4d688b8dfa6d1ecc
dkdam <dkdam@live.com>
2022-10-23 23:42:14 +1100

    added elixir

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -11,6 +11,7 @@ import supabase from "../public/assets/skills/Supabase.png"
 import firebase from "../public/assets/skills/Firebase.png"
 import go from "../public/assets/skills/golang.png"
 import python from "../public/assets/skills/python.png"
+import elixir from "../public/assets/skills/elixir.png"
 
 export const Skills = () => {
     return (
@@ -198,6 +199,22 @@ export const Skills = () => {
                                 </div>
                                 <div className='flex flex-col items-center justify-center'>
                                     <h3>GO</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                                <div className='m-auto'>
+                                    <Image
+                                        src={elixir}
+                                        alt="/"
+                                        width={64}
+                                        height={64}
+                                    />
+                                </div>
+                                <div className='flex flex-col items-center justify-center'>
+                                    <h3>Elixir</h3>
                                 </div>
                             </div>
                         </div>
