ba3c7cc7a13fa860a5e2432fdb61d82e01d492ff
dkdam <dkdam@live.com>
2023-01-13 14:18:33 +1100

    updated resume

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
     };
 
     const handleResume = () => {
-        window.open("https://drive.google.com/file/d/1e1vMvka15IU9zU-nPVkSTC9VgMd8R7tq/view");
+        window.open("https://drive.google.com/file/d/1RBDgSAM6HdwYLL3Nn5nhp7I8V0an5fgv/preview");
     };
 
     return (
@@ -50,38 +50,6 @@ const Contact = () => {
                         </div>
                     </div>
 
-                    {/* right */}
-                    {/* <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
-                        <div className='p-4'>
-
-                            <form>
-                                <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
-                                    <div className='flex flex-col'>
-                                        <label className='uppercase text-sm py-2'>Name</label>
-                                        <input className='border-2 rounded-lg p-3 flex border-gray 300' type="text" />
-                                    </div>
-                                    <div className='flex flex-col'>
-                                        <label className='uppercase text-sm py-2'>Phone Number</label>
-                                        <input className='border-2 rounded-lg p-3 flex border-gray 300' type="text" />
-                                    </div>
-                                </div>
-                                <div className='flex flex-col py-2'>
-                                    <label className='uppercase text-sm py-2'>Email</label>
-                                    <input className='border-2 rounded-lg p-3 flex border-gray 300' type="email" />
-                                </div>
-                                <div className='flex flex-col py-2'>
-                                    <label className='uppercase text-sm py-2'>Subject</label>
-                                    <input className='border-2 rounded-lg p-3 flex border-gray 300' type="text" />
-                                </div>
-                                <div className='flex flex-col py-2'>
-                                    <label className='uppercase text-sm py-2'>Message</label>
-                                    <textarea className='border-2 rounded-lg p-3 border-gray-300' rows='10'></textarea>
-                                </div>
-                                <button className='w-full p-4 text-gray-700 mt-4'>Send</button>
-                            </form>
-
-                        </div>
-                    </div> */}
                 </div>
                 <div className='flex justify-center py-12'>
                     <Link href='/'>
