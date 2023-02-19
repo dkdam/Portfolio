b832fa8c19d58149b675c1296436b6225844a764
dkdam <dkdam@live.com>
2023-02-19 18:25:07 +1100

    adding typescript

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -10,6 +10,7 @@ import tailwind from "../public/assets/skills/Tailwind.png"
 import supabase from "../public/assets/skills/Supabase.png"
 import firebase from "../public/assets/skills/Firebase.png"
 import python from "../public/assets/skills/python.png"
+import typescript from "../public/assets/skills/Typescript.png"
 
 export const Skills = () => {
     return (
@@ -181,6 +182,22 @@ export const Skills = () => {
                                 </div>
                                 <div className='flex flex-col items-center justify-center'>
                                     <h3>Python</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                                <div className='m-auto'>
+                                    <Image
+                                        src={typescript}
+                                        alt="/"
+                                        width={64}
+                                        height={64}
+                                    />
+                                </div>
+                                <div className='flex flex-col items-center justify-center'>
+                                    <h3>Typescript</h3>
                                 </div>
                             </div>
                         </div>
