52fdda649e86fc7153360abb3b44548bfac3b313
dkdam <dkdam@live.com>
2023-03-09 12:05:25 +1100

    updated about page

--- components/About.jsx
+++ components/About.jsx
@@ -19,8 +19,7 @@ const About = () => {
                 <div className="col-span-2">
                     <p className="uppercase text-xl tracking-widest text-[#E6AA68]">About me?</p>
                     <h2 className="py-4">David Dam</h2>
-                    <p className="py-2 text-gray-600">I like to build things and solve problems as that is what I wake up to everyday which has led me into coding as I want to learn something new and bring something to the table. I believe things that are made simple and easy yet solid and robust are to me the best way to go and I am driven by making to all work. </p>
-                    <p className="py-2 text-gray-600">On my time off, I am either still coding at my local cafe and/or doing archery down at my local range. Scroll down further to know more about me.</p>
+                    <p className="py-2 text-gray-600">I am a highly motivated and skilled software engineer graduate with a background in customer service, sales, and analytics. With experience in the casino and sales industries, I possess a passion for building and solving things. With my curiosity and collaborative mindset, I tend to tinker into problems to find the best possible outcomes. I have spent over 500 hours coding through a Software Engineering immersive course at General Assembly, where I developed my skills in this field. I am detail-oriented and possess exceptional communication and computer skills, making me an effective team player. In my free time, I enjoy practicing archery and doing side projects. </p>
                 </div>
             </div>
         </div>
